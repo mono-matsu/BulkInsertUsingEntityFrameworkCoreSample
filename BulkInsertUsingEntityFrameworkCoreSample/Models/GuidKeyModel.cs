c8f1ac62fd7da3e38a4bbf31c34260de387bd8b4
mono-matsu <matsuko@mononoco.com>
2022-03-08 08:50:36 +0900

    Add Model

---
+++ BulkInsertUsingEntityFrameworkCoreSample/Models/GuidKeyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkInsertUsingEntityFrameworkCoreSample.Models
+{
+    public class GuidKeyModel
+    {
+        public Guid Id { get; set; }
+        public int Value { get; set; }
+
+    }
+}
---
+++ BulkInsertUsingEntityFrameworkCoreSample/Models/IntKeyModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkInsertUsingEntityFrameworkCoreSample.Models
+{
+    public class IntKeyModel
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+
+    }
+}
