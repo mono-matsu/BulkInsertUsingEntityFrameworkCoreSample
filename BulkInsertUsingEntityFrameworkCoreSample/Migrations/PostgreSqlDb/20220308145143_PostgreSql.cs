ab1a8c50b7559b962e9b4f4fa70c35468354dbec
mono-matsu <matsuko@mononoco.com>
2022-03-09 00:12:33 +0900

    Generate migrations
    dotnet ef migrations add PostgreSql --project .\BulkInsertUsingEntityFrameworkCoreSample\BulkInsertUsingEntityFrameworkCoreSample.csproj --context PostgreSqlDbContext

---
+++ BulkInsertUsingEntityFrameworkCoreSample/Migrations/PostgreSqlDb/20220308145143_PostgreSql.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BulkInsertUsingEntityFrameworkCoreSample.Migrations.PostgreSqlDb
+{
+    public partial class PostgreSql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GuidKeyModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuidKeyModels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IntKeyModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IntKeyModels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GuidKeyModels");
+
+            migrationBuilder.DropTable(
+                name: "IntKeyModels");
+        }
+    }
+}
