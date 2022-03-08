6cca444fef0cc8f7c741eb5cd3c48a0442c74964
mono-matsu <matsuko@mononoco.com>
2022-03-08 09:22:49 +0900

    Generate migrations
    dotnet ef migrations add MySql --project .\BulkInsertUsingEntityFrameworkCoreSample\BulkInsertUsingEntityFrameworkCoreSample.csproj

---
+++ BulkInsertUsingEntityFrameworkCoreSample/Migrations/20220308001845_MySql.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkInsertUsingEntityFrameworkCoreSample.Migrations
+{
+    public partial class MySql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "GuidKeyModels",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuidKeyModels", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "IntKeyModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IntKeyModels", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GuidKeyModels");
+
+            migrationBuilder.DropTable(
+                name: "IntKeyModels");
+        }
+    }
+}
