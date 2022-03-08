5187a82a7bd67b550b506d735da372fe0ae52ebd
mono-matsu <matsuko@mononoco.com>
2022-03-08 09:19:45 +0900

    Add MySqlDbContext

---
+++ BulkInsertUsingEntityFrameworkCoreSample/Models/MySqlDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkInsertUsingEntityFrameworkCoreSample.Models
+{
+    public class MySqlDbContext : DbContext
+    {
+        public MySqlDbContext(DbContextOptions<MySqlDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GuidKeyModel> GuidKeyModels { get; set; }
+        public DbSet<IntKeyModel> IntKeyModels { get; set; }
+    }
+}
--- BulkInsertUsingEntityFrameworkCoreSample/Program.cs
+++ BulkInsertUsingEntityFrameworkCoreSample/Program.cs
@@ -1,3 +1,6 @@
+using BulkInsertUsingEntityFrameworkCoreSample.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContextPool<MySqlDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("MySql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), optionsBuilder => optionsBuilder.MinBatchSize(2)).LogTo(Console.WriteLine, LogLevel.Information);
+});
 
 var app = builder.Build();
 
